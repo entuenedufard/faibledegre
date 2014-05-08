6afe8c7ed9583ef49856b742165de484b718dc40
Antoine Defoort <entuenedufard@free.fr>
2014-05-08 13:40:32 +0200

    euh ben je sais plus heu, j'ai changé les formules de calcul pour que ce soit mieux

--- sondage/urls.py
+++ sondage/urls.py
@@ -6,6 +6,8 @@ from sondage import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^form/$', views.form, name='form'),
+    url(r'^form/(?P<ouiNonSliderValue>\d+)$', views.form, name='form'),
     url(r'^resultats/$', views.resultats, name='resultats'),
     url(r'^redir/$', views.redir, name='redir'),
     url(r'^control/$', views.control, name='control'),
--- sondage/views.py
+++ sondage/views.py
@@ -8,6 +8,9 @@ from sondage.forms import ReponseForm
 from .models import Reponse, Statut
 
 def index(request):
+    return render(request, 'sondage/index.html')
+
+def form(request, ouiNonSliderValue=0):
     statutResultat = Statut.objects.get(label="resultat").statut
     deactive = False
     if not (statutResultat=="active"):
@@ -19,13 +22,14 @@ def index(request):
         if form.is_valid(): #on compte les points
             points = form.cleaned_data['points'] 
             question_pas_claire = form.cleaned_data['question_pas_claire'] 
-            ressources_insuffisantes = form.cleaned_data['ressources_insuffisantes']     
+            ressources_insuffisantes = form.cleaned_data['ressources_insuffisantes'] 
             form.save()
+            form = ReponseForm()  
         else:
             probleme = True    
     else: # Si ce n'est pas du POST, c'est probablement une requête GET
-            form = ReponseForm()         
-    return render(request, 'sondage/index.html', locals())
+            form = ReponseForm()       
+    return render(request, 'sondage/form.html', locals())
 
 def resultats(request):
     statut = Statut.objects.get(label="resultat").statut
@@ -35,30 +39,31 @@ def resultats(request):
     else:
         result_list = Reponse.objects.all()
         nb_reponses = len(result_list)
+        nb_suffrage_exprimes = 0
         total_oui = 0
         total_non = 0
-        total_pas_clair = 0.0
-        total_manque_ressource = 0.0
+        total_sp = 0
+        total_pas_legitime = 0.0
         ratio_oui = 0
         ratio_non = 0
         ratio_sp = 0
-        ratio_pas_clair = 0.0
-        ratio_manque_ressource = 0.0
+        ratio_legitimite = 100
         if not nb_reponses==0:
             for r in result_list:
-                if r.points < 0:
-                    total_non -= r.points
-                else:
-                    total_oui += r.points
-                if r.question_pas_claire:
-                    total_pas_clair += 1
-                if r.ressources_insuffisantes:
-                    total_manque_ressource += 1
-            ratio_oui = total_oui/nb_reponses
-            ratio_non = total_non/nb_reponses
-            ratio_sp = 100-ratio_oui-ratio_non
-            ratio_pas_clair = round(total_pas_clair/nb_reponses*100,2)
-            ratio_manque_ressource = round(total_manque_ressource/nb_reponses*100,2)
+                if r.question_pas_claire or r.ressources_insuffisantes :
+                    total_pas_legitime += 1
+                else :
+                    nb_suffrage_exprimes += 1
+                    if r.points < 0:
+                        total_non += -r.points
+                        total_sp += 100 + r.points 
+                    else:
+                        total_oui += r.points
+                        total_sp += 100 - r.points
+            ratio_oui = total_oui/nb_suffrage_exprimes
+            ratio_non = total_non/nb_suffrage_exprimes
+            ratio_sp = total_sp/nb_suffrage_exprimes
+            ratio_legitimite= round(100-total_pas_legitime/nb_reponses*100)
     return render(request, 'sondage/resultats.html', locals())
      
 def redir(request):
